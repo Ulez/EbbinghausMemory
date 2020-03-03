c1edb51670460788e34963938125f0469e719429
Ulez <1104128773@qq.com>
2020-03-03 15:50:32 +0800

    主APP依赖于base模块；

---
+++ app/src/main/java/comulez/github/ebbinghausmemory/Main2Activity.java
@@ -0,0 +1,33 @@
+package comulez.github.ebbinghausmemory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import fun.learnlife.base.utils.Utils;
+import okhttp3.internal.Util;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        findViewById(R.id.tv_time).setOnClickListener(this);
+        findViewById(R.id.tv_translate).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_time:
+                Toast.makeText(this,"time",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.tv_translate:
+                Toast.makeText(this,"transtate",Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+}
